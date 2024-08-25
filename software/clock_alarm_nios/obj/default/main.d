ea4a4143d353a6f5d33f2a36bb93588dade4ede0
Kevin Lobo <67599399+gabrielwolfw@users.noreply.github.com>
2024-08-24 21:58:37 -0600

    cambios en el main

--- software/clock_alarm_nios/obj/default/main.d
+++ software/clock_alarm_nios/obj/default/main.d
@@ -1,6 +1,20 @@
 obj/default/main.o: main.c timer.h \
- ../clock_alarm_nios_bsp/HAL/inc/alt_types.h
+ ../clock_alarm_nios_bsp/HAL/inc/alt_types.h \
+ ../clock_alarm_nios_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../clock_alarm_nios_bsp//HAL/inc/io.h \
+ ../clock_alarm_nios_bsp//HAL/inc/alt_types.h \
+ ../clock_alarm_nios_bsp/system.h ../clock_alarm_nios_bsp/linker.h
 
 timer.h:
 
 ../clock_alarm_nios_bsp/HAL/inc/alt_types.h:
+
+../clock_alarm_nios_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../clock_alarm_nios_bsp//HAL/inc/io.h:
+
+../clock_alarm_nios_bsp//HAL/inc/alt_types.h:
+
+../clock_alarm_nios_bsp/system.h:
+
+../clock_alarm_nios_bsp/linker.h:
